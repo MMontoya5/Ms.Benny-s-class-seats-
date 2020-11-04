ede2b2d403efd9eff460a1ae9115bdda8f431bc1
MMontoya5 <mateo.montoya@patelhs.org>
2020-11-04 17:34:22 +0000

    Added scanner

--- Main.java
+++ Main.java
@@ -1,12 +1,16 @@
-class Main 
-{
-  public static void main(String[] args)
-  { 
-   int [] seats = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-   String [] names = new String[15];
-   for (int i = 0; i < names.length; i++)
-  { 
-   System.out.println( names[i] ); 
-  }
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    int[] seats = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+    String[] names = new String[15];
+
+    for (int i = 0; i < names.length; i++) 
+    {
+      Scanner scan = new Scanner(System.in); 
+      System.out.println("What is you name");
+      String name = scan.next();
+      System.out.println( name + "sits in seat"); 
+    }
   }
 }

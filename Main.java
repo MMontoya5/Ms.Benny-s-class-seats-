d0ba33cd7f96da66aed8ecc74a46b4ce3d3c2130
MMontoya5 <mateo.montoya@patelhs.org>
2020-11-06 15:38:59 +0000

    Added comments

--- Main.java
+++ Main.java
@@ -2,20 +2,20 @@ import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
-    int[] seats = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
-    String[] names = new String[15];
+    int[] seats = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };//Number of seats in class
+    String[] names = new String[15]; //Number of students in class 
 
-    Scanner scan = new Scanner(System.in); 
+    Scanner scan = new Scanner(System.in);// Scanner added to get user's name 
 
     for (int i = 0; i < names.length; i++) 
     {
-      System.out.println("What is you name");
+      System.out.println("What is you name");//asking user to give name
       names [i] = scan.next();   
     }
     int i = 0;
     while (seats [i] < names.length)
     {
-     System.out.println( names [i] + " sits in seat " + seats [i]);  
+     System.out.println( names [i] + " sits in seat " + seats [i]);//assigns seat to the user's name  
      i++;
     }
   }
